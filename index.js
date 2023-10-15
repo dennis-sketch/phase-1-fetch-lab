a46eb3ad5fb1735fc3f41d1020065acb6f94bbc2
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-10-15 22:53:36 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error(`Network response was not ok: ${resp.status}`);
+      }
+      return resp.json();
+    })
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error("Error fetching books:", error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const booksContainer = document.getElementById("books-container");
+
+  if (!booksContainer) {
+    console.error("Books container not found");
+    return;
+  }
+
+  booksContainer.innerHTML = '';
+
+  books.forEach((book) => {
+  
+    const bookName = book.name || "Unknown Title";
+    const listItem = document.createElement("li");
+    listItem.textContent = bookName;
+    booksContainer.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks);
